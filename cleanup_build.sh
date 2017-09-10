31046823c4f989c5a604fe23418cd078830c83bb
Max Vyz <mvyznyuk@gmail.com>
2017-09-10 21:36:32 +0300

    Fix CI deployment
    * Setup deployment configs
    
    * Change mode for cleanup script
    
    * Fix cleanup script
    
    * Update build script
    
    * Fix cleanup script

--- cleanup_build.sh
+++ cleanup_build.sh
@@ -2,13 +2,6 @@ FILES=$(find . -maxdepth 1 -type f)
 
 rm -rf src
 
-for dn in $FILES; do
-    if ($dn != 'build')
-      echo "Directory $dn will be removed";
-      rm -rf $dn;
-    done
-done
-
 for fn in $FILES; do
     echo "$fn will be removed";
     rm $fn;
